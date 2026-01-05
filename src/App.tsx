dcb07cce4e0097198712e21bbdada54a294695e3
Sanjay N <sanjayn29.aiml@gmail.com>
2026-01-05 21:20:00 +0530

    remove the unwanted

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { useState, useEffect } from 'react';
-import LoadingAnimation from './components/LoadingAnimation';
+import { useEffect } from 'react';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
-import FeaturedCollections from './components/FeaturedCollections';
-import ProductGallery from './components/ProductGallery';
-import BespokeSection from './components/BespokeSection';
-import CraftsmanshipSection from './components/CraftsmanshipSection';
 import Footer from './components/Footer';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
   useEffect(() => {
     document.title = 'Lumière - Fine Jewellery & Luxury Craftsmanship';
   }, []);
 
   return (
-    <>
-      {loading && <LoadingAnimation onComplete={() => setLoading(false)} />}
-      {!loading && (
-        <div className="min-h-screen bg-black">
-          <Navbar />
-          <HeroSection />
-          <FeaturedCollections />
-          <ProductGallery />
-          <BespokeSection />
-          <CraftsmanshipSection />
-          <Footer />
-        </div>
-      )}
-    </>
+    <div className="min-h-screen bg-black">
+      <Navbar />
+      <HeroSection />
+      <Footer />
+    </div>
   );
 }
 
